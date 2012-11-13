7364cef2b621ff8c3a93ccba4d1d72e87d1fb620
Nathan Alden <visualcsharpcoder@hotmail.com>
2012-11-12 19:06:45 -0600

    Properly followed semantic versioning and updated minor version numbers due to signature additions

--- Source/ViewEngines.RazorEngine/Properties/AssemblyInfo.cs
+++ Source/ViewEngines.RazorEngine/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly:AssemblyTitle("Junior.Route.ViewEngines.Razor")]
-[assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyFileVersion("1.0.0.0")]
+[assembly:AssemblyVersion("1.1.0.0")]
+[assembly:AssemblyFileVersion("1.1.0.0")]

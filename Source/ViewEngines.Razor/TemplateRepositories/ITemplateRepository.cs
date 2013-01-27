14fde78ca4785cc63e5d978109dd5ce41b6010ea
Nathan Alden <visualcsharpcoder@hotmail.com>
2013-01-27 12:50:43 -0600

    Renamed relativePath to key to communicate a more generic use for ITemplateRepository

--- Source/ViewEngines.Razor/TemplateRepositories/ITemplateRepository.cs
+++ Source/ViewEngines.Razor/TemplateRepositories/ITemplateRepository.cs
@@ -4,38 +4,38 @@ namespace Junior.Route.ViewEngines.Razor.TemplateRepositories
 {
 	public interface ITemplateRepository
 	{
-		TTemplate Get<TTemplate>(string relativePath, IEnumerable<string> namespaceImports)
+		TTemplate Get<TTemplate>(string key, IEnumerable<string> namespaceImports)
 			where TTemplate : ITemplate;
 
-		TTemplate Get<TTemplate>(string relativePath)
+		TTemplate Get<TTemplate>(string key)
 			where TTemplate : ITemplate;
 
-		TTemplate Get<TTemplate, TModel>(string relativePath, TModel model, IEnumerable<string> namespaceImports)
+		TTemplate Get<TTemplate, TModel>(string key, TModel model, IEnumerable<string> namespaceImports)
 			where TTemplate : ITemplate<TModel>;
 
-		TTemplate Get<TTemplate, TModel>(string relativePath, TModel model)
+		TTemplate Get<TTemplate, TModel>(string key, TModel model)
 			where TTemplate : ITemplate<TModel>;
 
-		Template Get(string relativePath, IEnumerable<string> namespaceImports);
-		Template Get(string relativePath);
-		Template<TModel> Get<TModel>(string relativePath, TModel model, IEnumerable<string> namespaceImports);
-		Template<TModel> Get<TModel>(string relativePath, TModel model);
+		Template Get(string key, IEnumerable<string> namespaceImports);
+		Template Get(string key);
+		Template<TModel> Get<TModel>(string key, TModel model, IEnumerable<string> namespaceImports);
+		Template<TModel> Get<TModel>(string key, TModel model);
 
-		string Run<TTemplate>(string relativePath, IEnumerable<string> namespaceImports)
+		string Run<TTemplate>(string key, IEnumerable<string> namespaceImports)
 			where TTemplate : ITemplate;
 
-		string Run<TTemplate>(string relativePath)
+		string Run<TTemplate>(string key)
 			where TTemplate : ITemplate;
 
-		string Run<TTemplate, TModel>(string relativePath, TModel model, IEnumerable<string> namespaceImports)
+		string Run<TTemplate, TModel>(string key, TModel model, IEnumerable<string> namespaceImports)
 			where TTemplate : ITemplate<TModel>;
 
-		string Run<TTemplate, TModel>(string relativePath, TModel model)
+		string Run<TTemplate, TModel>(string key, TModel model)
 			where TTemplate : ITemplate<TModel>;
 
-		string Run(string relativePath, IEnumerable<string> namespaceImports);
-		string Run(string relativePath);
-		string Run<TModel>(string relativePath, TModel model, IEnumerable<string> namespaceImports);
-		string Run<TModel>(string relativePath, TModel model);
+		string Run(string key, IEnumerable<string> namespaceImports);
+		string Run(string key);
+		string Run<TModel>(string key, TModel model, IEnumerable<string> namespaceImports);
+		string Run<TModel>(string key, TModel model);
 	}
 }

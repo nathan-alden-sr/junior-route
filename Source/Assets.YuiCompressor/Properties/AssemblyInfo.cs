89abe169f9984899592f5c6a8bc37740a684af9c
Nathan Alden <visualcsharpcoder@hotmail.com>
2013-01-27 13:15:07 -0600

    Fixed NuGet package dependency versions

--- Source/Assets.YuiCompressor/Properties/AssemblyInfo.cs
+++ Source/Assets.YuiCompressor/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly:AssemblyTitle("Junior.Routing.Assets.YuiCompressor")]
 [assembly:AssemblyVersion("2.1.0.0")]
-[assembly:AssemblyFileVersion("2.1.0.0")]
+[assembly:AssemblyFileVersion("2.1.1.0")]

3bafbd7832d65d712ef5b17b61b5ceb55641e795
Nathan Alden, Sr <me@nathanalden.com>
2013-06-01 20:53:59 -0500

    Updated JuniorCommon to 3.1.0 Updated Microsoft.AspNet.Razor to 2.0.30506.0

--- Source/AutoRouting/Properties/AssemblyInfo.cs
+++ Source/AutoRouting/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly:AssemblyTitle("Junior.Route.AutoRouting")]
-[assembly:AssemblyVersion("6.0.0.0")]
-[assembly:AssemblyFileVersion("6.0.0.0")]
+[assembly:AssemblyVersion("6.1.0.0")]
+[assembly:AssemblyFileVersion("6.1.0.0")]
--- Source/ViewEngines.Razor/Properties/AssemblyInfo.cs
+++ Source/ViewEngines.Razor/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly:AssemblyTitle("Junior.Route.ViewEngines.Razor")]
-[assembly:AssemblyVersion("4.0.0.0")]
-[assembly:AssemblyFileVersion("4.0.0.0")]
+[assembly:AssemblyVersion("4.1.0.0")]
+[assembly:AssemblyFileVersion("4.1.0.0")]

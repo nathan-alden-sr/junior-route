d9102786f821a44fab12f65298fbfb75bba52934
Nathan Alden, Sr <me@nathanalden.com>
2013-06-05 21:18:07 -0500

    Added FormContentTypeAttribute to encapsulate forcing only form-related Content-Type headers on POST methods

--- Source/AutoRouting/Properties/AssemblyInfo.cs
+++ Source/AutoRouting/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly:AssemblyTitle("Junior.Route.AutoRouting")]
-[assembly:AssemblyVersion("7.0.0.0")]
-[assembly:AssemblyFileVersion("7.0.2.0")]
+[assembly:AssemblyVersion("7.2.0.0")]
+[assembly:AssemblyFileVersion("7.2.0.0")]
---
+++ Source/AutoRouting/RestrictionMappers/Attributes/FormContentTypeAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Junior.Route.AutoRouting.RestrictionMappers.Attributes
+{
+	public class FormContentTypeAttribute : HeaderAttribute
+	{
+		public FormContentTypeAttribute()
+			: base("Content-Type", "^(application/x-www-form-urlencoded|multipart/form-data)(;|$)", RequestValueComparer.CaseInsensitiveRegex)
+		{
+		}
+	}
+}

a56359fb162e6777e163985e0fcd03ab8c93e648
Nathan Alden, Sr <me@nathanalden.com>
2013-09-29 21:22:52 -0500

    Added immutable responses for JSON content sent as text/html and text/plain (browser compatibility)

---
+++ Source/Routing/Responses/Text/JsonHtmlResponse.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Junior.Route.Routing.Responses.Text
+{
+	public class JsonHtmlResponse : ImmutableResponse
+	{
+		public JsonHtmlResponse(Func<byte[]> content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(Func<byte[]> content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(Func<string> content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(Func<string> content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(byte[] content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(byte[] content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(string content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(string content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(object content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().Content(() => JsonConvert.SerializeObject(content)), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(object content, JsonSerializerSettings serializerSettings)
+			: base(new Response().TextHtml().Content(() => JsonConvert.SerializeObject(content, serializerSettings)))
+		{
+		}
+
+		public JsonHtmlResponse(object content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextHtml().ContentEncoding(encoding).Content(() => JsonConvert.SerializeObject(content)), configurationDelegate)
+		{
+		}
+
+		public JsonHtmlResponse(object content, Encoding encoding, JsonSerializerSettings serializerSettings)
+			: base(new Response().TextHtml().ContentEncoding(encoding).Content(() => JsonConvert.SerializeObject(content, serializerSettings)))
+		{
+		}
+	}
+}
---
+++ Source/Routing/Responses/Text/JsonPlainResponse.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Junior.Route.Routing.Responses.Text
+{
+	public class JsonPlainResponse : ImmutableResponse
+	{
+		public JsonPlainResponse(Func<byte[]> content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(Func<byte[]> content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(Func<string> content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(Func<string> content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(byte[] content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(byte[] content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(string content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(string content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().ContentEncoding(encoding).Content(content), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(object content, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().Content(() => JsonConvert.SerializeObject(content)), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(object content, JsonSerializerSettings serializerSettings)
+			: base(new Response().TextPlain().Content(() => JsonConvert.SerializeObject(content, serializerSettings)))
+		{
+		}
+
+		public JsonPlainResponse(object content, Encoding encoding, Action<Response> configurationDelegate = null)
+			: base(new Response().TextPlain().ContentEncoding(encoding).Content(() => JsonConvert.SerializeObject(content)), configurationDelegate)
+		{
+		}
+
+		public JsonPlainResponse(object content, Encoding encoding, JsonSerializerSettings serializerSettings)
+			: base(new Response().TextPlain().ContentEncoding(encoding).Content(() => JsonConvert.SerializeObject(content, serializerSettings)))
+		{
+		}
+	}
+}
